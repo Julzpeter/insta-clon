128dd6e293e0e90e7553245728d0d2065525badc
Juliet <chepngetichjuliet@gmail.com>
2019-07-24 14:21:20 +0300

    creates likes models

--- instagram/models.py
+++ instagram/models.py
@@ -100,3 +100,13 @@ class Followers(models.Model):
     Follower = models.CharField(max_length=40, default='')
 
 
+class Likes(models.Model):
+    post = models.ForeignKey('Image')
+    user = models.ForeignKey(User)
+
+    class Meta:
+        unique_together = ("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' ' + self.post.title
+    
