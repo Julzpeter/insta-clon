7a42f5c6b9ad8e78141955da4ce4a07f6d91ea9e
Juliet <chepngetichjuliet@gmail.com>
2019-07-24 14:43:13 +0300

    creates urls for the app

---
+++ instagram/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^search/', views.get_search, name='get_search'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^update/profile/(\d+)$', views.update_profile, name='update-profile'),
+    url(r'^post/image/(\d+)$', views.post_image, name='post_image'),
+    url(r'^post/comment/(\d+)$', views.post_comment, name='post_comment'),
+]
